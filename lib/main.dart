7f5c209870ae2d9dc829caf3d6a29e9e3ad26144
Nalin Aswani <38874717+Nalinboi@users.noreply.github.com>
2020-06-30 22:51:37 +1200

    Uploaded assets
    some stiuff i had not committed before

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,9 @@
 
 import 'package:draft_gym_app/clickbait_grid.dart';
 import 'package:draft_gym_app/theme.dart';
+import 'package:draft_gym_app/week_grid.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
-import 'clickbait_container.dart';
-
 
 void main() => runApp(MyApp());
 
@@ -23,10 +21,26 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<ThemeChanger>(
-      builder: (_) => ThemeChanger(ThemeData.dark()),
-      child: MyAppWithTheme(),
-    );
+    return MaterialApp(
+          title: 'Flutter login UI',
+          theme: ThemeData(
+            // This is the theme of your application.
+            //
+            // Try running your application with "flutter run". You'll see the
+            // application has a blue toolbar. Then, without quitting the app, try
+            // changing the primarySwatch below to Colors.green and then invoke
+            // "hot reload" (press "r" in the console where you ran "flutter run",
+            // or simply save your changes to "hot reload" in a Flutter IDE).
+            // Notice that the counter didn't reset back to zero; the application
+            // is not restarted.
+            primarySwatch: Colors.blue,
+          ),
+          home: LoginPage(title: 'Flutter Login'),
+        );
+    // return ChangeNotifierProvider<ThemeChanger>(
+      // builder: (_) => ThemeChanger(ThemeData.dark()),
+      // child: MyAppWithTheme(),      
+    // );
   }
 }
 
@@ -34,10 +48,7 @@ class MyAppWithTheme extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Provider.of<ThemeChanger>(context);
-    return MaterialApp(
-      home: MyStatefulWidget(),
-      theme: theme.getTheme(),
-    );
+    return MaterialApp( home: MyStatefulWidget(), theme: theme.getTheme() );
   }
 }
 
@@ -53,30 +64,19 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
   static List<Widget> _widgetOptions = <Widget>[
     ClickbaitGrid(), //makes it work the same way, but seperates into other widgets. 
-    Text(
-      'Index 1: Business',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 2: School',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 3: Profile',
-      style: optionStyle,
-    ),
+    WeekGrid(),
+    // Text('Index 1: Business', style: optionStyle),
+    Text('Index 2: School', style: optionStyle),
+    Text('Index 3: Profile', style: optionStyle),
   ];
 
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
+  //when item tapped we display whatever outof the widget array list above
+  void _onItemTapped(int index) { setState(() {_selectedIndex = index; }); }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+      appBar: AppBar( //the top widget bar
         title: const Text('GYM SHIT ONLY'),                
         backgroundColor: Color.fromARGB(100, 0, 0, 0),
       ),
@@ -86,22 +86,10 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
       bottomNavigationBar: BottomNavigationBar(
         unselectedItemColor: Colors.grey[800],
         items: const <BottomNavigationBarItem>[          
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            title: Text('Home'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            title: Text('Business'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.fastfood),
-            title: Text('School'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.portrait),
-            title: Text('Profile'),
-          ),
+          BottomNavigationBarItem( icon: Icon(Icons.home), title: Text('Series')),
+          BottomNavigationBarItem( icon: Icon(Icons.business), title: Text('Plan')),
+          BottomNavigationBarItem( icon: Icon(Icons.fastfood), title: Text('Nutrition')),
+          BottomNavigationBarItem( icon: Icon(Icons.portrait), title: Text('Profile')),
         ],
         currentIndex: _selectedIndex,
         selectedItemColor: Colors.amber[800],
@@ -112,5 +100,99 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
     );
   }
 }
+//==============================My App Widget ====================================//
 
+//==============================Login page ====================================//
+class LoginPage extends StatefulWidget {
+  LoginPage({Key key, this.title}) : super(key: key);
+
+  final String title;
+  @override
+  _MyLoginPageState createState() => _MyLoginPageState();
+}
+
+class _MyLoginPageState extends State<LoginPage> {
+   TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
+
+      @override
+      Widget build(BuildContext context) {
+
+        final emailField = TextField(
+          obscureText: false,
+          style: style,
+          decoration: InputDecoration(
+              contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+              hintText: "Email",
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+        );
+        final passwordField = TextField(
+          obscureText: true,
+          style: style,
+          decoration: InputDecoration(
+              contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+              hintText: "Password",
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+        );
+        final loginButon = Material(
+          elevation: 5.0,
+          borderRadius: BorderRadius.circular(30.0),
+          color: Colors.black87,
+          child: MaterialButton(
+            minWidth: MediaQuery.of(context).size.width,
+            padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+            onPressed: () {
+              // Scaffold.of(context).showSnackBar(SnackBar(content: Text('Logged in')));              
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => MyStatefulWidget()),
+                // MaterialPageRoute(builder: (context) => MyAppWithTheme()),
+                // MaterialPageRoute(builder: (context) => SecondRoute()), 
+              );
+            },
+            child: Text("Login",
+                textAlign: TextAlign.center,
+                style: style.copyWith(
+                    color: Colors.white, fontWeight: FontWeight.bold)),
+          ),
+        );
+
+        return Scaffold(
+          body: Center(
+            child: Container(
+              color: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.all(36.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(
+                      height: 155.0,
+                      child: Image.asset(
+                        "Assets/Images/ewock.jpg",
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                    SizedBox(height: 45.0),
+                    emailField,
+                    SizedBox(height: 25.0),
+                    passwordField,
+                    SizedBox(
+                      height: 35.0,
+                    ),
+                    loginButon,
+                    SizedBox(
+                      height: 15.0,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        );
+      }
+}
+//==============================Login page ====================================//
 
